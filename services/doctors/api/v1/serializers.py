cbe690325a089b178c0a7fc5cffa8e6acee75c51
krumitz <ahmid.balogun@shipa.ng>
2024-03-08 07:08:38 +0100

    mod: lint fixes

--- services/doctors/api/v1/serializers.py
+++ services/doctors/api/v1/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from core.models import Doctor
 
 
-class DoctorSerializer():
-    
+class DoctorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Doctor
         fields = "__all__"
